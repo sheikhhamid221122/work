4f3539a1400d6c7dc297166545699a09484970e7
Abdul Hannan <abdulhannan.hamid@gmail.com>
2026-01-29 17:30:07 +0500

    salauddin added

--- app.py
+++ app.py
@@ -389,7 +389,7 @@ def generate_form_invoice():
         elif username == "8255820":
             template_name = "invoice_templatezahid.html"
             print(f"Selected template: {template_name} for username 8255820")
-        elif username in ["3075270", "0946915", "7542425", "2853653", "B690329", "3520271603355", "3556084"]:
+        elif username in ["3075270", "0946915", "7542425", "2853653", "B690329", "3520271603355", "3556084", "3520229157309"]:
             template_name = "invoice_template3.html"  # Shared template for these users
             print(f"Selected template: {template_name} for username: {username}")
         else:
@@ -1170,7 +1170,7 @@ def submit_fbr():
                 template_name = "invoice_zeeshanst.html"
             elif username == "7542425":
                 template_name = "invoice_template3.html"
-            elif username in ["3075270", "0946915", "2853653", "B690329", "3520271603355", "3556084"]:
+            elif username in ["3075270", "0946915", "2853653", "B690329", "3520271603355", "3556084", "3520229157309"]:
                 template_name = "invoice_template3.html"
             else:
                 template_name = "invoice_template2.html"
@@ -1403,7 +1403,7 @@ def generate_invoice_excel():
             template_name = "invoice_zeeshanst.html"
         elif username == "7542425":
             template_name = "invoice_template3.html"
-        elif username in ["3075270", "B690329", "3520271603355", "3556084"]:
+        elif username in ["3075270", "B690329", "3520271603355", "3556084", "3520229157309"]:
             template_name = "invoice_template3.html"  # Use appropriate template for Care Pharmaceuticals
         else:
             template_name = "invoice_template3.html"
